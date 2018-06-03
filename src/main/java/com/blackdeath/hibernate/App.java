e6e02f7e3bc6b4efc140f90bc0ea3e067f516986
Seth Karim Luis Martínez <ing.seth.luis@gmail.com>
2018-06-03 07:24:11 -0500

    Se agrega a hibernate.cfg.xml las entidades mapeadas a la base de datos

--- src/main/java/com/blackdeath/hibernate/App.java
+++ src/main/java/com/blackdeath/hibernate/App.java
@@ -1,13 +1,23 @@
 package com.blackdeath.hibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+		Configuration configuration = new Configuration();
+		configuration.configure();
+
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+		Session session = sessionFactory.openSession();
+
+	}
 }
